a2fef1686cde7840a03f91a6f3826373c8fc8d41
Towhedul <towhidshamim009@gmail.com>
2025-07-02 08:29:11 +0600

    middlewire add for module active

--- Modules/HRIS/routes/web.php
+++ Modules/HRIS/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Middleware\ModuleActive;
 use Illuminate\Support\Facades\Route;
 use Modules\HRIS\Http\Controllers\HRISController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified',ModuleActive::class.':hris'])->group(function () {
     Route::resource('hris', HRISController::class)->names('hris');
 });
