e018b9ea58b5464f9dc0fa63e62385442438d664
Towhedul <towhidshamim009@gmail.com>
2025-08-07 14:34:37 +0600

    update

--- bootstrap/cache/modules.php
+++ bootstrap/cache/modules.php
@@ -1,9 +1,15 @@
 <?php return array (
   'providers' => 
   array (
+    0 => 'Modules\\HRIS\\Providers\\HRISServiceProvider',
+    1 => 'Modules\\Inventory\\Providers\\InventoryServiceProvider',
+    2 => 'Modules\\Payroll\\Providers\\PayrollServiceProvider',
   ),
   'eager' => 
   array (
+    0 => 'Modules\\HRIS\\Providers\\HRISServiceProvider',
+    1 => 'Modules\\Inventory\\Providers\\InventoryServiceProvider',
+    2 => 'Modules\\Payroll\\Providers\\PayrollServiceProvider',
   ),
   'deferred' => 
   array (
