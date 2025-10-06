eaf49e1f33622d52f80a1f73b1f686388dd4cb65
Towhedul <towhidshamim009@gmail.com>
2025-10-06 11:36:11 +0600

    maternity entry

--- Modules/HRIS/app/Models/Tools/MaternityEntry.php
+++ Modules/HRIS/app/Models/Tools/MaternityEntry.php
@@ -4,6 +4,11 @@ namespace Modules\HRIS\Models\Tools;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
+use Modules\HRIS\Models\Setup\Department;
+use Modules\HRIS\Models\Database\Employee;
+use Modules\HRIS\Models\Setup\Designation;
+use Modules\HRIS\Models\Setup\Organization;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Modules\HRIS\Database\Factories\Tools\MaternityEntryFactory;
 
@@ -30,8 +35,23 @@ class MaternityEntry extends Model
         });
     }
 
-    // protected static function newFactory(): Tools\MaternityEntryFactory
-    // {
-    //     // return Tools\MaternityEntryFactory::new();
-    // }
+    public function employeeBasic() : BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'employee_id', 'employee_id');
+    }
+
+    public function designation() : BelongsTo
+    {
+        return $this->belongsTo(Designation::class);
+    }
+
+    public function department() : BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function organization() : BelongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
 }
